9a91f02bd74c4f6059050c3bc7f64f8ea9ad72e5
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-30 14:12:58 +0000

    add file

--- js/index.js
+++ js/index.js
@@ -16,3 +16,26 @@ $(function() {
 
     })
 })
+
+/*
+$(function() {
+    $('#btn').click(function(e) {
+        e.preventDefault();
+        var num = $('#number').val();
+
+        function fib(num) {
+            var a = 1,
+                b = 1;
+            for (var i = 3; i <= num; i++) {
+                var c = a + b;
+                a = b;
+                b = c;
+            }
+            return b;
+        }
+        var res = fib(num);
+        $('#result').html(res);
+
+    })
+})
+*/
